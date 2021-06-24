a5e72add0a9a8deb39cf329105a9bcbea63804b3
tpsalo <tpsalo@utu.fi>
2021-06-24 23:01:57 +0300

    Exercise 1.3 done

--- tehtava1.1/src/App.js
+++ tehtava1.1/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
   
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
       <Header course={course} />
       
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
 
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
       
     </div>
   )
@@ -25,11 +31,8 @@ const App = () => {
 
 const Header = (props) => {
 
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
+  return <h1>{props.course}</h1>
+  
 }
 
 
@@ -65,4 +68,5 @@ const Part = (props) => {
     </div>
   )
 }
+
 export default App

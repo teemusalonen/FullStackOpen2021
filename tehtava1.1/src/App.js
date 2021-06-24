6692066830a2fe6c98156c5a4728d57f00ac9113
tpsalo <tpsalo@utu.fi>
2021-06-24 23:57:38 +0300

    Exercise 1.5 done

--- tehtava1.1/src/App.js
+++ tehtava1.1/src/App.js
@@ -2,29 +2,31 @@ import React from 'react'
 
 const App = () => {
   
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }  
 
   return (
     <div>
       <Header course={course} />
       
-      <Content parts={parts} />
+      <Content parts={course.parts} />
 
-      <Total parts={parts} />
+      <Total parts={course.parts} />
       
     </div>
   )
@@ -33,7 +35,7 @@ const App = () => {
 
 const Header = (props) => {
 
-  return <h1>{props.course}</h1>
+  return <h1>{props.course.name}</h1>
   
 }
 

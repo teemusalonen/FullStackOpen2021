65dd8dda4de017f5ea89a19d22a2dce159a543b3
tpsalo <tpsalo@utu.fi>
2021-06-27 22:41:20 +0300

    Tehtävä 1.7 tehty

--- tehtavat1.6-1.11/src/App.js
+++ tehtavat1.6-1.11/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 
-function App() {
+const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = bad+good+neutral
+  const average = (good-bad)/all
+  const positive = good/all*100
 
   return (
     <div>
@@ -23,6 +26,12 @@ function App() {
       neutral {neutral}
       <br/>
       bad {bad}
+      <br/>
+      all {all}
+      <br/>
+      average {average}
+      <br/>
+      positive {positive}%
     </div>  
 
   );

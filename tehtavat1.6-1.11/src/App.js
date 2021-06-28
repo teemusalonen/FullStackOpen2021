63316a8536d5c0cd09ec49c23039ffdec32abb74
tpsalo <tpsalo@utu.fi>
2021-06-28 18:19:18 +0300

    Tehtävä 1.11 korjattu, tehtävä nyt valmis

--- tehtavat1.6-1.11/src/App.js
+++ tehtavat1.6-1.11/src/App.js
@@ -8,6 +8,23 @@ const Button = ({text, handleClick}) => {
   )
 }
 
+const StatisticLine = ({text, value}) => {
+  if(text === 'positive'){
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}%</td>
+      </tr>
+    )
+  }  
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 
 const Statistics = ({good, neutral, bad}) => {
   const all = bad+good+neutral
@@ -27,26 +44,11 @@ const Statistics = ({good, neutral, bad}) => {
       <h1>statistics</h1>
       <table>
         <tbody>
-          <tr>
-            <td>good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>  
-            <td>neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>average</td>
-            <td>{average}</td>
-          </tr>
-          <tr>
-            <td>positive</td>
-            <td>{positive}%</td>
-          </tr>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive} />
         </tbody>
       </table>
     </div>
